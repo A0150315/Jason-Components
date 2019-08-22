d91f4af5cc21776e65bb4c83e5f339059fbc783b
JasonTan <574469551@qq.com>
2019-08-22 20:30:30 +0800

    chore:absolutePath

---
+++ src/components/audio-player/index.tsx
@@ -0,0 +1,11 @@
+import React, { memo } from 'react'
+
+const Audio = memo<{src?:string}>(function Audio(props) {
+    return (
+        <div>
+            1
+        </div>
+    )
+})
+
+export default Audio
